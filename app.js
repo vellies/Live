fa9396adbcea4160c3beb275521e5c59680cfbf9
vellies <vellies6113@gmail.com>
2019-08-15 22:48:45 +0530

    03-15.08.19 express middleware

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-console.log('Welcome Vellies');
-console.log('Hello Vellies');
+const express = require('express');
+const app = express();
+
+//add Middleware 
+// it has 3 arguments like req,res,next
+app.use((req, res, next) => {
+    console.log('Test Next Function');
+    next();//allow the requestto continue to the next middleware in line
+});
+
+app.use((req, res, next) => {
+    console.log('This is second Middleware');
+    res.send('Hello From Express');
+    next();
+});
+
+app.listen(3000);
