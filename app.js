652fb56b5da7fd4eaff93b9c63de4825ff079f3c
vellies <vellies6113@gmail.com>
2019-08-18 13:30:37 +0530

    06180819 404page and url filtering

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRouter);
+app.use('/admin', adminRouter);
 app.use(shopRouter);
+
+//Create 404 error Page
+app.use((req, res, next)=> {
+    res.status(404).send('<h1>Page Not Found</h1>');
+});
 
 app.listen(3000);//created http server in expressjs
--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 //add Middleware 
 // it has 3 arguments like req,res,next
+///admin/add-product =>GET
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title" id="title"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" id="title"><button type="submit">Add Product</button></form>');
 });
-
-router.post('/product',(req, res, next) => {
+///admin/add-product =>POST
+router.post('/add-product',(req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
