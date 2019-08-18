62b6deaf16e432421dd1a2edb8af13a11016c3cb
vellies <vellies6113@gmail.com>
2019-08-18 15:12:32 +0530

    09180819 add util path file

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router();
+const rootDir = require('../util/path');
 
 //add Middleware 
 // it has 3 arguments like req,res,next
 ///admin/add-product =>GET
 router.get('/add-product',(req, res, next) => {
-    res.sendFile(path.join(__dirname, '../','views','add-product.html'));
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 ///admin/add-product =>POST
 router.post('/add-product',(req, res, next) => {
