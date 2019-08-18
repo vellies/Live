9c85049a57f16129f066e2db4066890cbc0ef60d
vellies <vellies6113@gmail.com>
2019-08-18 14:41:30 +0530

    08170819 rendering 404 page with path

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -12,7 +13,7 @@ app.use(shopRouter);
 
 //Create 404 error Page
 app.use((req, res, next)=> {
-    res.status(404).send('<h1>Page Not Found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);//created http server in expressjs
