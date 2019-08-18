f88804d7a8d1c55977fac426e349584dcbfb25cf
vellies <vellies6113@gmail.com>
2019-08-18 14:29:53 +0530

    07170819 create & serving HTML page

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
@@ -5,7 +6,7 @@ const router = express.Router();
 // it has 3 arguments like req,res,next
 ///admin/add-product =>GET
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" id="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname, '../','views','add-product.html'));
 });
 ///admin/add-product =>POST
 router.post('/add-product',(req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/',(req, res, next) => {
-    res.send('Hello From Express');//like express github lib file html
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
