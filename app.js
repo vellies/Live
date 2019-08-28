1e7f7d0fcca9f4ffcbc0ce84b94606147125c2ff
vellies <vellies6113@gmail.com>
2019-08-28 08:12:01 +0530

    15280819 Sharing data across requests

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-const adminRouter = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRouter);
+app.use('/admin', adminData.routes);
 app.use(shopRouter);
 
 //Create 404 error Page
