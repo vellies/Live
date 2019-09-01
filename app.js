1f4409d565d37415ff80ef161700611987f3149f
vellies <vellies6113@gmail.com>
2019-09-01 20:25:03 +0530

    010919-1 just started with ejs Template

--- app.js
+++ app.js
@@ -2,20 +2,11 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
-app.engine(
-    'hbs',
-    expressHbs({
-      layoutsDir: 'views/layouts/',
-      defaultLayout: 'main-layout',
-      extname: 'hbs'
-    })
-  );
-  app.set('view engine', 'hbs');
+  app.set('view engine', 'ejs');
   app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
