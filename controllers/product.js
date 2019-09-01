6c283f2eab2698dabbcea36d10fd27faf6278da9
vellies <vellies6113@gmail.com>
2019-09-02 00:19:09 +0530

    010919-1 adding Product model

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', { 
@@ -10,12 +10,13 @@ exports.getAddProduct = (req, res, next) => {
   };
 
 exports.postAddProduct = (req, res, next) => {
-  // console.log(req.body);
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('shop', {
     prods: products,
     pageTitle: 'Shop',
