370228a6287ca86915310f30867fa39c31ffa3fb
hai <vellieswaran@rayi.in>
2019-09-04 22:10:34 +0530

    040919-2 Editing and Deleting Products

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const description = req.body.description;
   const price = req.body.price;
-  const product = new Product(title, imageUrl, price, description, price);
+  const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
