927618e645776eeac072570139e1893ba1e342ad
hai <vellieswaran@rayi.in>
2019-09-10 01:06:16 +0530

    090919-2 Compected For synamic content data

--- models/cart.js
+++ models/cart.js
@@ -43,11 +43,14 @@ module.exports = class cart{
     }
 static deleteProduct(id, productPrice) {
     fs.readFile(p, (err, fileContent)=> {
-        if(!err){
+        if(err){
             return;
         }
         const updatedCart = { ...JSON.parse(fileContent) };
         const product = updatedCart.products.find(prod => prod.id === id);
+        if(!product){
+            return;
+        }
         const productQty = product.qty;
         updatedCart.products = updatedCart.products.filter(
            prod => prod.id !== id
