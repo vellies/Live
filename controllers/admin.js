84f7a78c5cc9feafeca588d1bac6cd5373fdf0cd
hai <vellieswaran@rayi.in>
2019-09-09 22:54:19 +0530

    090919-2 Deleting Cart Items

--- controllers/admin.js
+++ controllers/admin.js
@@ -68,4 +68,6 @@ exports.getProduct = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) =>{
     const prodId = req.body.productId;
+    Product.deleteById(prodId);
+    res.redirect('/admin/products');
 };
--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const cart = require('./cart');
 
 const p = path.join(
     path.dirname(process.mainModule.filename),
@@ -49,10 +50,11 @@ module.exports = class Product {
 
     static deleteById(id) {
         getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id);
             const updatedProducts = products.filter(prod => prod.id === id);
             fs.writeFile(p, JSON.stringify(updatedProducts), err =>{
                 if(!err){
-                    
+                   cart.deleteProduct(id, product.price);
                 }
             });
         });
