885d8395858f3269edbf8417a08025d512d7a9dd
hai <vellieswaran@rayi.in>
2019-09-10 23:35:09 +0530

    100919-2 sql create, connect database and retrive data

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 
+const db = require('./util/database');
+
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
   app.set('view engine', 'ejs');
   app.set('views', 'views');
+
+  db.execute('SELECT * FROM products')
+  .then(result=>{
+    console.log(result[0], result[1]);
+  })
+  .catch(err=>{
+    console.log(err);
+  });
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
