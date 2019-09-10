886f5f89605008a7dad22835fe0f00fd9420d67e
hai <vellieswaran@rayi.in>
2019-09-11 00:09:49 +0530

    100919-2 Insertin data into database

--- models/product.js
+++ models/product.js
@@ -10,7 +10,10 @@ module.exports = class Product {
         this.price = price;
     }
     save() {
-
+        return db.execute(
+            'INSERT INTO products (title, price, imageUrl, description) VALUES(?, ?, ?, ?)',
+            [this.title, this.price, this.imageUrl, this.description]
+        );
     }
 
     static deleteById(id) {
