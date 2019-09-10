4806333f8bf33653ebb8980f1fed17d932662fd7
hai <vellieswaran@rayi.in>
2019-09-11 00:19:04 +0530

    100919-2 Fetching Single Product

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,13 +17,16 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   // console.log(prodId);
-  Product.findById(prodId, product => {
+  Product.findById(prodId)
+  .then(([product])=>{
+    console.log(product);
     res.render('shop/product-detail', {
-      product: product,
+      product: product[0],
       pageTitle: product.title,
       path:'/products'
     });
-  });
+  })
+  .catch(err=>console.log(err));
   // res.redirect('/');
 }
 
