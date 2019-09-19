60b5fd9e9ff3a51b4c1fa3586eaca9ad732056eb
hai <vellieswaran@rayi.in>
2019-09-20 00:17:47 +0530

    190919-2 connect to the database in sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,19 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-live',
-    password: ''
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'node-live',
+//     password: ''
+// });
+
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('node-live','root','',{
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
