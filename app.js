24a54552eb6e376b32d7d369bf6e9c9793f55a96
hai <vellieswaran@rayi.in>
2019-09-20 00:53:47 +0530

    190919-2 sync js definition

--- app.js
+++ app.js
@@ -2,23 +2,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const errorController = require('./controllers/error');
+const errorController = require('./controllers/error');   
 
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
   app.set('view engine', 'ejs');
   app.set('views', 'views');
-
-  // db.execute('SELECT * FROM products')
-  // .then(result=>{
-  //   console.log(result[0], result[1]);
-  // })
-  // .catch(err=>{
-  //   console.log(err);
-  // });
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,4 +21,11 @@ app.use(shopRouter);
 //Create 404 error Page
 app.use(errorController.get404);
 
-app.listen(3000);//created http server in expressjs
+sequelize.sync().then(result =>{
+  console.log(result);
+  app.listen(3000);
+})
+.catch(err => {
+  console.log(err);
+});
+
