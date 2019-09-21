36fcdbef9fc6ffdb780cb84babb220f11a850f5e
hai <vellieswaran@rayi.in>
2019-09-21 19:57:00 +0530

    210919-2 One-tomany & Many-To-Many Relation

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
@@ -37,10 +39,15 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
 
 sequelize
-// .sync({ force: true })
-.sync()
+.sync({ force: true })
+// .sync()
 .then(result =>{
   // console.log(result);
   return User.findByPk(1);
