f107d773a5b8dd9484eef7faa41e20b5b7e366eb
vellies <vellies6113@gmail.com>
2019-09-29 16:18:46 +0530

    290919-1 Cloud mongoDB connection

--- app.js
+++ app.js
@@ -3,16 +3,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');   
+const mongoConnect = require('./util/database');
 
-const sequelize = require('./util/database');
 
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
 
-const adminRouter = require('./routes/admin');
-const shopRouter = require('./routes/shop');
+// const adminRouter = require('./routes/admin');
+// const shopRouter = require('./routes/shop');
 
   app.set('view engine', 'ejs');
   app.set('views', 'views');
@@ -21,52 +17,24 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
  app.use((req, res, next) => {
-   User.findByPk(1)
-   .then(user => {
-     req.user =user;
-     next();
-   })
-   .catch(err => {
-     console.log(err);
-   });
+  //  User.findByPk(1)
+  //  .then(user => {
+  //    req.user =user;
+  //    next();
+  //  })
+  //  .catch(err => {
+  //    console.log(err);
+  //  });
  });
 
-app.use('/admin', adminRouter);
-app.use(shopRouter);
+// app.use('/admin', adminRouter);
+// app.use(shopRouter);
 
 //Create 404 error Page
 app.use(errorController.get404);
 
-Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
-
- 
-sequelize
-// .sync({ force: true })
-.sync()
-.then(result =>{
-  // console.log(result);
-  return User.findByPk(1);
-})
-.then(user => {
-  if (!user){
-    return User.create({name: 'vellies', email: 'vellies6113@gmail.com'})
-  }
-  return user;
-})
-.then(user => {
-  // console.log(user);
-  return user.createCart();
-  
-})
-.then(cart => {
+//callback function
+mongoConnect(client => {
+  console.log(client);
   app.listen(3000);
-})
-.catch(err => {
-  console.log(err);
 });
-
--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-live','root','',{
-    dialect: 'mysql',
-    host: 'localhost'
-});
+//Error function
+const MongoConnect = (callback) =>{
+    MongoClient.connect(
+        'mongodb+srv://vellies:vellies113.@live01-jjkmg.mongodb.net/test?retryWrites=true&w=majority')
+        .then(client=> {
+            console.log('Successfully connected Cloud MongoDB!');
+            callback(client);//client means access database connection
+        }).catch(err => { 
+            console.log(err); 
+        });
+};
 
-module.exports = sequelize;
+module.exports = MongoConnect;
