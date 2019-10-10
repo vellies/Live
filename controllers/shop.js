b12be9cf76914ef2148ca60ca5e5811da3b776bd
vellies <vellies6113@gmail.com>
2019-10-10 08:50:25 +0530

    101019-1N Displaying Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,109 +1,83 @@
 const Product = require('../models/product');
-// const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(products => {
+  Product.fetchAll()
+  .then(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
-  }).catch(err=>{
+  })
+  .catch(err => {
     console.log(err);
   });
-  
-}
+  };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  // Product.findAll({ where: { id: prodId} })
-  // .then(products => {
-  //   res.render('shop/product-detail', {
-  //     product: products[0],
-  //     pageTitle: products[0].title,
-  //     path:'/products'
-  //   });
-  // })
-  // .catch(err => {
-  //   console.log(err);
+  // Product.findAll({ where: { id: prodId } })
+  //    .then(products => {
+  //      res.render('shop/product-detail', {
+  //        product: products[0],
+  //        pageTitle: products[0].title,
+  //        path: '/products'
+  //        });
+  //    })
+  //    .catch(err => {console.log(err);
   // });
   // console.log(prodId);
-  Product.findByPk(prodId)
-  .then(product=>{
+  Product.findById(prodId)
+  .then(product => {
     // console.log(product);
   res.render('shop/product-detail', {
     product: product,
     pageTitle: product.title,
-    path:'/products'
+    path: '/products'
   });
   })
-  .catch(err=>console.log(err));
+  .catch(err => console.log(err));
   // res.redirect('/');
-}
+};
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll().then(products => {
+  Product.fetchAll()
+  .then(products => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
-  }).catch(err=>{
-    console.log(err);
-  });
-}
-
-exports.getCart = (req, res, next) => { 
-     console.log(req.user.cart);
-  req.user.getCart()
-  .then(cart => {
-    return cart.getProducts()
-    .then(products => {
-       res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: products
-      });     
-    })
-    .catch(err => {
-      console.log(err);
-    });
   })
   .catch(err => {
     console.log(err);
   });
-  // Cart.getCart(cart => {
-  //   Product.fetchAll(products => {
-  //     // console.log(products);
-  //     const cartProducts = [];
-  //     for (product of products) {
-  //       const cartProductData=cart.products.find(prod => prod.id === product.id);
-  //       // console.log('cartProductDataNew',cartProductData);
-  //       if(cartProductData){
-  //         cartProducts.push({productData: product, qty: cartProductData.qty});
-  //       }
-  //     }
-  //     // console.log('A', cartProducts);
-  //     res.render('shop/cart', {
-  //       path: '/cart',
-  //       pageTitle: 'Your Cart',
-  //       products: cartProducts
-  //     });
-  //   });
-    
-  // });
+};
 
-}
+exports.getCart = (req, res, next) => {
+  req.user
+    .getCart()
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
+    })
+    .catch(err => console.log(err));
+};
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId).then(product => {
+  Product.findById(prodId)
+  .then(product => {
     return req.user.addToCart(product);
   })
   .then(result => {
     console.log(result);
-  })
-  .catch(err => { console.log(err) });
+    res.redirect('/cart');
+  });
+  //.catch(err => { console.log(err) });
   // Product.findById(prodId, product => {
   //   console.log(product.price);
   //   Cart.addProduct(prodId, product.price);
--- models/user.js
+++ models/user.js
@@ -4,31 +4,37 @@ const getDb = require('../util/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email, cart, id){
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
-    this.cart = cart;
+    this.cart = cart; // {items: []}
     this._id = id;
   }
+
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this);
   }
 
-  addToCart(product){
-    const cartProductindex = this.cart.items.findIndex(cp => {
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
       return cp.productId.toString() === product._id.toString();
     });
     let newQuantity = 1;
     const updatedCartItems = [...this.cart.items];
-    if(cartProductindex >= 0){
-      newQuantity = this.cart.items[cartProductindex].quantity + 1;
-      updatedCartItems[cartProductindex].quantity = newQuantity;
-    }else{
-      updatedCartItems.push({ productId: new ObjectId(product._id), quantity: newQuantity});
-    }
     
-    const updatedCart = { items: updatedCartItems };
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({ 
+        productId: new ObjectId(product._id),
+        quantity: newQuantity
+      });
+    }
+    const updatedCart = { 
+      items: updatedCartItems 
+    };
     const db = getDb();
     return db
       .collection('users')
@@ -38,7 +44,28 @@ class User {
       );
   }
 
-  static findById(userId){
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => { 
+      return i.productId; 
+    });
+    return db
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {
+            ...p,
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
+        });
+      });
+  }
+ 
+  static findById(userId) {
     const db = getDb();
     return db
     .collection('users')
@@ -46,7 +73,10 @@ class User {
     .then(user => {
       console.log(user);
       return user;
-    });
+    })
+    .catch(err => {
+      console.log(err);
+    });  
   }
 }
 
