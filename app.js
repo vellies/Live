2c2392b4cc4c0afd626e49fbc23100a2c26a37ad
vellies <vellies6113@gmail.com>
2019-10-17 09:06:01 +0530

    171719-1N Saving data through Mongoose

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 const app = express();
 // const bodyParser = require('body-parser');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const errorController = require('./controllers/error');   
 const mongoConnect = require('./util/database').MongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
@@ -22,16 +24,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
- app.use((req, res, next) => {
-   User.findById("5d9dee281c9d44000081a2ad")
-   .then(user => {
-     req.user = new User(user.name, user.email, user.cart, user._id);
-     next();
-   })
-   .catch(err => {
-     console.log(err);
-   });
- });
+//  app.use((req, res, next) => {
+//    User.findById("5d9dee281c9d44000081a2ad")
+//    .then(user => {
+//      req.user = new User(user.name, user.email, user.cart, user._id);
+//      next();
+//    })
+//    .catch(err => {
+//      console.log(err);
+//    });
+//  });
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
@@ -40,6 +42,13 @@ app.use(shopRouter);
 app.use(errorController.get404);
 
 //callback function
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+.connect(
+  'mongodb+srv://vellies:vellies113.@live01-jjkmg.mongodb.net/Live?retryWrites=true&w=majority'
+  )
+  .then(result=>{
+    app.listen(3000)
+  })
+  .catch(err=>{
+    console.log(err);
+  });
