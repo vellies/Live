4a9b3a39c3470fe1299967d87777d20e780a1219
vellies <vellies6113@gmail.com>
2019-10-18 09:22:46 +0530

    181019-1N user mode

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');   
 const mongoConnect = require('./util/database').MongoConnect;
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
@@ -24,16 +24,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//  app.use((req, res, next) => {
-//    User.findById("5d9dee281c9d44000081a2ad")
-//    .then(user => {
-//      req.user = new User(user.name, user.email, user.cart, user._id);
-//      next();
-//    })
-//    .catch(err => {
-//      console.log(err);
-//    });
-//  });
+ app.use((req, res, next) => {
+   User.findById("5da9356b0abb3915c0544fbc")
+   .then(user => {
+     req.user = user;
+     next();
+   })
+   .catch(err => {
+     console.log(err);
+   });
+ });
 
 app.use('/admin', adminRouter);
 app.use(shopRouter);
@@ -47,6 +47,18 @@ mongoose
   'mongodb+srv://vellies:vellies113.@live01-jjkmg.mongodb.net/Live?retryWrites=true&w=majority'
   )
   .then(result=>{
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Vellies',
+          email: 'velies6113@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });    
     app.listen(3000)
   })
   .catch(err=>{
