4074f1ee1a8ff1cb4fc75c7aeb84aa8602e0894f
vellies <vellies6113@gmail.com>
2019-10-20 16:28:52 +0530

    signUp page added

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
     });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
 exports.postLogin = (req, res, next) => {
     User.findById('5da9f168c5d6640fb4156936')
       .then(user => {
@@ -20,6 +28,8 @@ exports.postLogin = (req, res, next) => {
       })
       .catch(err => console.log(err));
   };
+
+exports.postSignup = (req, res, next) => {};
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
