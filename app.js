3a3ef171072ccc56626576990788b7120007c022
vellies <vellies6113@gmail.com>
2019-10-20 16:50:08 +0530

    Created new Live User

--- app.js
+++ app.js
@@ -57,18 +57,6 @@ app.use(errorController.get404);
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Vellies',
-          email: 'velies6113@gmail.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
