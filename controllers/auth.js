2e394595e05cd93abc77695569738db293a31476
vellies <vellies6113@gmail.com>
2019-10-20 09:04:04 +0530

    201019-1N Session in middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,17 @@
 exports.getLogin = (req, res, next) => {
-    console.log(req.get('Cookie').trim().split('=')[1]);
+    const isLoggedIn = req
+    .get('Cookie')
+    .trim()
+    .split('=')[1] === 'true';
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: isLoggedIn
     });
 };
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn = true');
+    // res.setHeader('Set-Cookie', 'loggedIn = true');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 };
