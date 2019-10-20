f52d3d54fede716ae4acc0fbb694d262f7edf990
vellies <vellies6113@gmail.com>
2019-10-20 10:26:44 +0530

    Using MDB to store session

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://vellies:vellies113.@live01-jjkmg.mongodb.net/Live';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -19,7 +26,14 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'my secret', resave: false, saveUninitialized: false }));
+app.use(
+  session({ 
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
   User.findById("5da9f168c5d6640fb4156936")
@@ -38,7 +52,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://vellies:vellies113.@live01-jjkmg.mongodb.net/Live?retryWrites=true&w=majority'
+    MONGODB_URI
   )
   .then(result => {
     User.findOne().then(user => {
