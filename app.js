1fbc1e11007e68668f9e16d4ba0f17440135d5a1
vellies <vellies6113@gmail.com>
2019-10-20 19:38:55 +0530

    Using CSURFTOKEN

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csurf = require('csurf');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -17,6 +18,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+const csrfProduction = csurf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -35,6 +37,7 @@ app.use(
     store: store
   })
 );
+app.use(csrfProduction);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
