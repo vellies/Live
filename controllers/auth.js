9d44a794f323cd77d444ada4ed1dc8eb9bd06ab8
vellies <vellies6113@gmail.com>
2019-10-23 09:19:36 +0530

    update email page

--- controllers/auth.js
+++ controllers/auth.js
@@ -114,3 +114,16 @@ exports.postLogout = (req, res, next) => {
     });
 };
  
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+};
